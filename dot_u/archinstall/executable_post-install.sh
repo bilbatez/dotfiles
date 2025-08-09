281113eb44728c135d0c6e4fcf45b4fd5bdfa773
Bilbatez <bilbatez@pm.me>
2025-08-09 17:08:15 +0800

    Update archinstall post install script

--- dot_u/archinstall/executable_post-install.sh
+++ dot_u/archinstall/executable_post-install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Run this script post Arch Installation
 sudo ip link set wlan0 up
 echo "wireless network interface is up!"
 
@@ -8,11 +9,21 @@ echo "wireless regdb updated!"
 sudo iw reg set SG
 echo "wireless regdom updated!"
 
-sudo mkswap -U clear --size 8G --file /swapfile
+sudo dd if=/dev/urandom of=/etc/cryptsetup-keys.d/cryptswap.key bs=1024 count=4
+sudo chmod 0400 /etc/cryptsetup-keys.d/cryptswap.key
+echo "swapfile keyfile created!"
+
+sudo mkswap -U clear --size 16G --file /swapfile
 sudo cryptsetup luksFormat /swapfile
 sudo cryptsetup open /swapfile cryptswap
 sudo mkswap /dev/mapper/cryptswap
 sudo swapon /dev/mapper/cryptswap
-sudo "\n\n#Swapfile\n/dev/mapper/cryptswap none swap sw 0 0\n" >> /etc/fstab
-sudo "\n\n#Swapfile\ncryptswap /swapfile none luks\n" >> /etc/crypttab
 echo "swapfile created!"
+
+sudo cryptsetup luksAddKey /swapfile /etc/cryptsetup-keys.d/cryptswap.key
+sudo "\n\n#Swapfile\ncryptswap /swapfile /etc/cryptsetup-keys.d/cryptswap.key luks\n" >> /etc/crypttab
+sudo "\n\n#Swapfile\n/dev/mapper/cryptswap none swap defaults 0 0\n" >> /etc/fstab
+sudo mkinitcpio -P
+echo "swapfile configured!"
+
+echo "Reboot Recommended!"
