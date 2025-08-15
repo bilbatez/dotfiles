9c49aac3a8e7d450ddc1bd82d547ba43730cdfdc
Bilbatez <bilbatez@pm.me>
2025-08-16 01:11:38 +0800

    Update bash prompt style & alias sh

--- dot_u/scripts/executable_alias.sh
+++ dot_u/scripts/executable_alias.sh
@@ -55,7 +55,76 @@ alias xc="xclip -selection clipboard"
 alias xv="xclip -o -selection clipboard"
 
 #VPN
-alias dvpnon="sudo wg-quick up Entropy-SG-171"
-alias dvpnoff="sudo wg-quick down Entropy-SG-171"
-alias vpnon="sudo wg-quick up"
-alias vpnoff="sudo wg-quick down"
+default_vpn="Entropy-SG-171"
+alias dvpnon="sudo wg-quick up $default_vpn"
+alias dvpnoff="sudo wg-quick down $default_vpn"
+alias dsvpnon="sudo systemctl start wg-quick@$default_vpn"
+alias dsvpnoff="sudo systemctl stop wg-quick@$default_vpn"
+alias dsvpnsta="sudo systemctl status wg-quick@$default_vpn"
+
+declare -A vpn_confs
+vpn_confs=(
+    [scmm]="EntropySC-CH-MM-2"
+    [schk]="EntropySC-CH-HK-2"
+    [thk]="EntropyT-HK-27-TOR"
+)
+
+_is_default_vpn_on() {
+    sudo systemctl is-active --quiet wg-quick@$default_vpn
+    return $?
+}
+
+_get_vpn_config() {
+    choice="$1"
+    if [[ -v vpn_confs["$choice"] ]]; then
+        echo "${vpn_confs[$choice]}" 
+        return 0
+    else
+        echo "VPN Config Not Found!"
+        return 1
+    fi
+}
+
+vpnon() {
+    choice="$1"
+    config=$(_get_vpn_config "$choice")
+    exit_code=$?
+    if [[ $exit_code != 0 ]]; then
+        echo "$config"
+        return $exit_code
+    fi
+
+    _is_default_vpn_on
+    exit_code=$?
+    if [[ $exit_code != 0 ]]; then
+        echo "Default VPN service is on! Please shut it down."
+        return $exit_code
+    fi
+
+    $(sudo wg-quick up $config)
+    exit_code=$?
+    if [[ $exit_code != 0 ]]; then
+        echo "Error on wg-quick down $config!"
+        return $exit_code
+    fi
+    echo "Connected to $config!"
+}
+
+vpnoff() {
+    choice="$1"
+    config=$(_get_vpn_config "$choice")
+    exit_code=$?
+    if [[ $exit_code != 0 ]]; then
+        echo "$config"
+        return $exit_code
+    fi
+
+    $(sudo wg-quick down $config)
+    exit_code=$?
+    if [[ $exit_code != 0 ]]; then
+        echo "Error on wg-quick up $config!"
+        return $exit_code
+    fi
+    echo "Disconnected from $config!"
+
+}
