8d1ebbd30a6f6cae4e0146fae5eb518569f169f9
Bilbatez <bilbatez@pm.me>
2025-12-03 18:25:29 +0800

    chore: update JAVA_HOME for macos

--- dot_u/scripts/executable_env.sh
+++ dot_u/scripts/executable_env.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 home_dir=$HOME
 u_dir=$HOME/.u
+
+case "$OSTYPE" in
+linux-gnu*) export JAVA_HOME="/usr/lib/jvm/java-openjdk" ;;
+darwin*) export JAVA_HOME="/opt/homebrew/opt/openjdk" ;;
+esac
+
 system_env_variables=(
   "${home_dir}/.cargo/bin"
   "${home_dir}/.local/share/coursier/bin"
   "${home_dir}/.u/bin"
+  "$JAVA_HOME/bin"
 )
 for env_variable in "${system_env_variables[@]}"; do
   case ":$PATH:" in
@@ -18,4 +25,3 @@ export U_DIR=$u_dir
 export U_SCRIPTS_DIR=$u_dir/scripts
 export TERMINAL=alacritty
 export MANPAGER="vim -M +MANPAGER -"
-export JAVA_HOME="/usr/lib/jvm/java-openjdk"
