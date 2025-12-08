6fe8cd5067385bbe04f313293f4c301a7345baf6
Bilbatez <bilbatez@pm.me>
2025-12-08 22:33:36 +0800

    chore(env): add bun env variables

--- dot_u/scripts/executable_env.sh
+++ dot_u/scripts/executable_env.sh
@@ -7,11 +7,18 @@ linux-gnu*) export JAVA_HOME="/usr/lib/jvm/java-openjdk" ;;
 darwin*) export JAVA_HOME="/opt/homebrew/opt/openjdk" ;;
 esac
 
+export U_DIR=$u_dir
+export U_SCRIPTS_DIR=$u_dir/scripts
+export TERMINAL=alacritty
+export MANPAGER="vim -M +MANPAGER -"
+export BUN_INSTALL="$HOME/.bun"
+
 system_env_variables=(
   "${home_dir}/.cargo/bin"
   "${home_dir}/.local/share/coursier/bin"
   "${home_dir}/.u/bin"
   "$JAVA_HOME/bin"
+  "$BUN_INSTALL/bin"
 )
 for env_variable in "${system_env_variables[@]}"; do
   case ":$PATH:" in
@@ -20,8 +27,3 @@ for env_variable in "${system_env_variables[@]}"; do
   esac
 done
 export PATH=$PATH
-
-export U_DIR=$u_dir
-export U_SCRIPTS_DIR=$u_dir/scripts
-export TERMINAL=alacritty
-export MANPAGER="vim -M +MANPAGER -"
