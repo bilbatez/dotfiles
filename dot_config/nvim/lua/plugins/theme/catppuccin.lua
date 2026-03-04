e6b569f28bc714db43501f87d2857276a04a6198
Bilbatez <bilbatez@pm.me>
2026-03-04 19:25:06 +0800

    chore(lazyvim): reinit config

---
+++ dot_config/nvim/lua/plugins/theme/catppuccin.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      auto_integrations = true,
+      flavour = "mocha",
+      lsp_styles = {
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.special.bufferline").get_theme()
+          end
+        end,
+      },
+    },
+  },
+}
